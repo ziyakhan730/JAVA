6d5ed356e0ba9eedffb714ef006812a9e8cbf077
Ziya <bcan1ca24093@itmuniversity.ac.in>
2025-07-17 12:09:09 +0530

    ...

---
+++ Program_3.java
@@ -0,0 +1,11 @@
+class Program_3 {
+    public static void main(String[] args) {
+int a=10;
+int b=20;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+}
+}
